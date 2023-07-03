8ef5c24efe30079b51c192f9f9149bd64c9d1db2
German Sanmillan <sanmillan.german@gmail.com>
2023-07-03 14:16:02 +0200

    SSH

---
+++ PasswordAttacks/RulesWordlists.sh
@@ -0,0 +1,1 @@
+hashcat -r <rulesfile> --stdout <wordlist> > <destwordlist> #You can use the rules hashcat sintax to add a set of rules over a password list and divert the result over a targetfile. 
--- PasswordAttacks/sshpassphrase.sh
+++ PasswordAttacks/sshpassphrase.sh
@@ -10,11 +10,21 @@ hashcat -m 22921 ssh.hash <wordlists> [-r ssh.rule] --force
 
 #If Hashcat doesn't support the hash algorith we can try with JohnTheRipper.
 
-john --wordlist=ssh.passwords --rules=sshRules ssh.hash
+john --wordlist=<wordlists> --rules=<rulename> <hashfile>
 
 #John the ripper store its rules in /etc/john/john.conf, by the label:
 
-[List.Rules:sshRules]
-c $1 $3 $7 $!
-c $1 $3 $7 $@
-c $1 $3 $7 $#
+[List.Rules:<rulesname>]
+rule1
+rule2
+...
+rulex
+
+#For example;
+
+# [List.Rules:sshRules]
+# c $1 $3 $7 $!
+# c $1 $3 $7 $@
+# c $1 $3 $7 $#
+
+#So you have to pass the contents above into the /etc/john/john.conf and then call the rule by the name of the label, in this case, sshRules.
