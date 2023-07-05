21c403908048206b01ab73547ce8c9476ca3f507
German Sanmillan <sanmillan.german@gmail.com>
2023-07-05 12:09:01 +0200

    fix

---
+++ PrivilegeEscalation/Windows/BasicEnumeration/InstalledApps.ps1
@@ -0,0 +1,3 @@
+Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" #<--- 32-bits applications.
+
+Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" #<--- 64-bits applications.
