8407d6838466a51b7709755adea16254db99260e
German Sanmillan <sanmillan.german@gmail.com>
2023-07-16 12:45:29 +0200

    fix

--- PrivilegeEscalation/Windows/BasicEnumeration/UserSecContext.ps1
+++ PrivilegeEscalation/Windows/BasicEnumeration/UserSecContext.ps1
@@ -7,7 +7,7 @@ whoami /priv <--- Privileges over the system owned by current user.
 net user <--- Accounts availables on Domain
 net user <USERNAME> / whoami /all <--- Describes full USERNAME context
 runas /user:<USERNAME> cmd <--- With GUI and credentials we can run a CMD as other user.
-
+net accounts <--- Expose information relative, among other things, password configurations.
 --------------------------------------------
 PowerShell
 
