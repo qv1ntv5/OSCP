68d0f5c52f0063f26273190bf1b6d7a82c890318
qv1ntv5 <sanmillan.german@gmail.com>
2023-10-15 04:17:53 -0400

    Commit

--- PrivilegeEscalation/Linux/BasicEnumeration/Services-Process.sh
+++ PrivilegeEscalation/Linux/BasicEnumeration/Services-Process.sh
@@ -1,3 +1,5 @@
-ps -aux <--- Snapshot about current running process in system.
-Packages Managers: (dpkg -l in debian for example). Displays installed aplications.
-find / -writable -type d 2>/dev/null <--- Writeable archives.
+ps -aux # <--- Snapshot about current running process in system.
+#Packages Managers: (dpkg -l in debian for example). Displays installed aplications.
+find / -writable -type d 2>/dev/null #<--- Writeable archives.
+
+#We also can use the pspy (https://github.com/DominicBreuker/pspy) tool to check the behaviour of the process without root permissions. Very useful to check scheduled tasks or things of the kind.
