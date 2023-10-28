6dba975f7dc2e92d1f5d7a0b7160eff10324da51
qv1ntv5 <sanmillan.german@gmail.com>
2023-10-28 08:16:21 -0400

    Commit

--- SubNets/PortForwarding/Windows/ChiselPortForwarding.bat
+++ SubNets/PortForwarding/Windows/ChiselPortForwarding.bat
@@ -1,4 +1,4 @@
-::In some ocasions (this is specifically true on the exam enviroment), some machines on the internal network such as MS02 can connect back with our kali. We have to do portforwarding through MS01 to make contact MS02 --> Kali.
+::In some ocasions (this is specifically true on the exam enviroment), some machines on the internal network such as MS02 can't connect back with our kali. We have to do portforwarding through MS01 to make contact MS02 --> Kali.
 ::In order to do this we use the following command with chisel.
 
 start /b chisel.exe client <tunnelip>:<tunelport> <listeningport>:<targetip>:<targetport>
