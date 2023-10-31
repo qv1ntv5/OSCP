c28d68cb6d7dfd6a64ac33058fa4009142c608f9
qv1ntv5 <sanmillan.german@gmail.com>
2023-10-31 07:44:51 -0400

    Commit

--- InformationGathering/SMB/CMEPassSpray.sh
+++ InformationGathering/SMB/CMEPassSpray.sh
@@ -1,5 +1,5 @@
-#THis command will try to enumerate valids credentials on a <ip>. The username/password fields also admits files. If we are trying to enumerate a machine on a AD set, it would be worth to add the [-d <domain>].
+# His command will try to enumerate valids credentials on a <ip>. The username/password fields also admits files. If we are trying to enumerate a machine on a AD set, it would be worth to add the [-d <domain>].
 
 crackmapexec smb <ip> -u <username/file> -p <password/file> [-d <domain>] --continue-on-success
 
-
+# We can also enumerate local users (in the context of an AD enviroment) by adding the hostname of the machine with the -d flag.
--- PrivilegeEscalation/Windows/CommPrivEscVectors/SAMDump.sh
+++ PrivilegeEscalation/Windows/CommPrivEscVectors/SAMDump.sh
@@ -1,1 +1,4 @@
 impacket-secretsdump -sam SAM -system SYSTEM LOCAL
+
+
+# This files are stored frecuently on C:\Windows\System32 directory but usually we can't reach them because are frecuently in use. We can dump its contents with mimikatz instead. However, if we find a windows.old directory we can attemp to download this files and his contents.
