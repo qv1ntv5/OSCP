67d841a74f110dffa351abeeed71f8efbb97fb14
Sr4nd3rs0n <73143623+Sr4nd3rs0n@users.noreply.github.com>
2024-08-15 13:05:58 +0200

    Update BinaryHijacking.ps1

--- PrivilegeEscalation/Windows/CommPrivEscVectors/BinaryHijacking/BinaryHijacking.ps1
+++ PrivilegeEscalation/Windows/CommPrivEscVectors/BinaryHijacking/BinaryHijacking.ps1
@@ -4,11 +4,11 @@
 #### icacls permissions mask ####
 
 # +Mask+++++++Permissions+
-#   F		       Full access
-#   M		       Modify access
-#   RX	       Read and execute access
-#   R		       Read-only access
-#   W		       Write-only access
+#   F          Full access
+#   M          Modify access
+#   RX         Read and execute access
+#   R          Read-only access
+#   W          Write-only access
 ######################################################################
 -------------------------------
 ######### From PowerShell #########
